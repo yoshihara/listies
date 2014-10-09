cb928cb2a5ae8302f44c59e6e714d6e59fd05eb2
yoshihara <h.yoshihara@everyleaf.com>
2014-10-09 21:13:07 +0900

    test: Add User#authentication tests

--- test/models/users_test.rb
+++ test/models/users_test.rb
@@ -1,4 +1,39 @@
 require 'test_helper'
 
 class UsersTest < ActiveSupport::TestCase
+  def test_first_login
+    user = authentication
+    assert_equal(user.uid, twitter_params['uid'])
+  end
+
+  def test_already_logined
+    params = {
+      uid:      twitter_params['uid'],
+      nickname: twitter_params['info']['nickname'],
+      name:     twitter_params['info']['name'],
+      image:    twitter_params['info']['image'],
+    }
+
+    User.new(params).save!
+    user = authentication
+    assert_equal(user.uid, twitter_params['uid'])
+  end
+
+  private
+
+  def authentication
+    User.authentication(twitter_params)
+  end
+
+  def twitter_params
+    {
+      'provider' => 'twitter',
+      'uid' => '123',
+      'info' => {
+        'nickname' => 'hyoshihara04',
+        'name' => 'hyoshihara',
+        'image' => 'https://pbs.twimg.com/profile_images/501327171231567872/hUi0EULf.jpeg'
+      }
+    }
+  end
 end
